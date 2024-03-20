5810d7725f4e730d8084705f020c8d119fe73757
xani1996 <rashidixaniar@gmail.com>
2024-03-20 14:29:32 +0330

    style share by email form and form.py

--- blog/forms.py
+++ blog/forms.py
@@ -3,10 +3,12 @@ from .models import PostComment
 
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=80)
-    email = forms.EmailField()
-    to = forms.EmailField()
-    comments = forms.CharField(required=False, widget=forms.Textarea)
+    name = forms.CharField(max_length=80, label='نام خود را وارد کنید',
+                           widget=forms.TextInput(attrs={'class': 'form-control form text-right'}))
+    email = forms.EmailField(label='ایمیل خود را وارد کنید', widget=forms.EmailInput(attrs={'class': 'form-control text-right'}))
+    to = forms.EmailField(label='ایمیل خود را وارد کنید', widget=forms.EmailInput(attrs={'class': 'form-control text-right'}))
+    comments = forms.CharField(required=False, widget=forms.Textarea(attrs={'class': 'form-control text-right'}),
+                               label='متن نظر خود را وارد کنید')
 
 
 class CommentForm(forms.ModelForm):
@@ -15,7 +17,9 @@ class CommentForm(forms.ModelForm):
         fields = ['name', 'email', 'body']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control text-right', 'placeholder': 'نام خود را وارد کنید'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control text-right', 'placeholder': 'ایمیل خود را وارد کنید'}),
-            'body': forms.Textarea(attrs={'class': 'form-control text-right', 'placeholder': 'متن نظر خود را وارد کنید'}),
+            'email': forms.EmailInput(
+                attrs={'class': 'form-control text-right', 'placeholder': 'ایمیل خود را وارد کنید'}),
+            'body': forms.Textarea(
+                attrs={'class': 'form-control text-right', 'placeholder': 'متن نظر خود را وارد کنید'}),
 
         }
--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -7,3 +7,4 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
